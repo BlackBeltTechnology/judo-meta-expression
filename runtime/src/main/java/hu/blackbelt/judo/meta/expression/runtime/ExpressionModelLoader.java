c690f6724f41ae98784911cb1a180bc5a7675d3b
Robert Csakany <robert.csakany@blackbelt.hu>
2019-04-03 16:00:37 +0200

    Add save method to Loader

--- runtime/src/main/java/hu/blackbelt/judo/meta/expression/runtime/ExpressionModelLoader.java
+++ runtime/src/main/java/hu/blackbelt/judo/meta/expression/runtime/ExpressionModelLoader.java
@@ -8,7 +8,9 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.URIHandler;
 import org.eclipse.emf.ecore.resource.impl.ResourceFactoryRegistryImpl;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMIResource;
 import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.URIHandlerImpl;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -75,6 +77,33 @@ public class ExpressionModelLoader {
             b.metaVersionRange(acceptedMetaVersionRange);
         }
         return b.build();
+    }
+
+    public static Map<Object, Object> getExpressionDefaultSaveOptions() {
+        final Map<Object, Object> saveOptions = new HashMap<>();
+        saveOptions.put(XMIResource.OPTION_DECLARE_XML,Boolean.TRUE);
+        saveOptions.put(XMIResource.OPTION_PROCESS_DANGLING_HREF,XMIResource.OPTION_PROCESS_DANGLING_HREF_DISCARD);
+        saveOptions.put(XMIResource.OPTION_URI_HANDLER, new URIHandlerImpl() {
+            @Override
+            public URI deresolve(URI uri) {
+                if (uri.hasFragment() && uri.hasOpaquePart() && baseURI.hasOpaquePart()) {
+                    if (uri.opaquePart().equals(baseURI.opaquePart())) {
+                        return URI.createURI("#" + uri.fragment());
+                    }
+                }
+                return super.deresolve(uri);
+            }
+        });
+        saveOptions.put(XMIResource.OPTION_SCHEMA_LOCATION,Boolean.TRUE);
+        saveOptions.put(XMIResource.OPTION_DEFER_IDREF_RESOLUTION,Boolean.TRUE);
+        saveOptions.put(XMIResource.OPTION_SKIP_ESCAPE_URI,Boolean.FALSE);
+        saveOptions.put(XMIResource.OPTION_ENCODING,"UTF-8");
+        return saveOptions;
+    }
+
+
+    public static void saveExpressionModel(ExpressionModel expressionModel) throws IOException {
+        expressionModel.getResourceSet().getResource(expressionModel.getUri(), false).save(getExpressionDefaultSaveOptions());
     }
 
 }
