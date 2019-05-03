9e6f4a275387cafafcbb0eb47b1b6e2dc21c76a3
Jozsef Borcsok <jozsef.borcsok@blackbelt.hu>
2019-05-03 14:33:37 +0200

    cleanup, enable all validations

--- runtime/src/main/java/hu/blackbelt/judo/meta/expression/runtime/ExpressionEvaluator.java
+++ runtime/src/main/java/hu/blackbelt/judo/meta/expression/runtime/ExpressionEvaluator.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
