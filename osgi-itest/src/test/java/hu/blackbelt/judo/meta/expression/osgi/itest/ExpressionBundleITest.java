6b66d8bae072997512adcb8c5f98a88204fed50a
Robert Csakany <robert.csakany@blackbelt.hu>
2019-08-22 11:07:11 +0200

    JNG-200 Add osgi integration smoke test - model tracker

---
+++ osgi-itest/src/test/java/hu/blackbelt/judo/meta/expression/osgi/itest/ExpressionBundleITest.java
@@ -0,0 +1,120 @@
+package hu.blackbelt.judo.meta.expression.osgi.itest;
+
+import hu.blackbelt.osgi.utils.osgi.api.BundleTrackerManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.ops4j.pax.swissbox.core.BundleUtils;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.log.LogService;
+
+import javax.inject.Inject;
+import java.io.*;
+
+import static hu.blackbelt.judo.meta.expression.osgi.itest.ExpressionKarafFeatureProvider.*;
+import static org.junit.Assert.*;
+import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+import static org.ops4j.pax.swissbox.core.BundleUtils.getBundle;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class ExpressionBundleITest {
+
+    public static final String HU_BLACKBELT_JUDO_META_EXPRESSION_OSGI = "hu.blackbelt.judo.meta.expression.osgi";
+    public static final String HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_MEASURE = "hu.blackbelt.judo.meta.expression.model.adapter.measure";
+    public static final String HU_BLACKBELT_JUDO_META = "hu.blackbelt.judo.meta";
+    public static final String HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_PSM = "hu.blackbelt.judo.meta.expression.model.adapter.psm";
+    public static final String HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_ASM = "hu.blackbelt.judo.meta.expression.model.adapter.asm";
+    public static final String HU_BLACKBELT_JUDO_META_MEASURE_OSGI = "hu.blackbelt.judo.meta.measure.osgi";
+    public static final String HU_BLACKBELT_JUDO_META_PSM_OSGI = "hu.blackbelt.judo.meta.psm.osgi";
+    public static final String HU_BLACKBELT_JUDO_META_ASM_OSGI = "hu.blackbelt.judo.meta.asm.osgi";
+
+    @Inject
+    LogService log;
+
+    @Inject
+    protected BundleTrackerManager bundleTrackerManager;
+
+    @Inject
+    BundleContext bundleContext;
+
+    @Configuration
+    public Option[] config() throws FileNotFoundException {
+
+        return combine(getRuntimeFeaturesForMetamodel(this.getClass()),
+                mavenBundle(maven()
+                        .groupId(HU_BLACKBELT_JUDO_META)
+                        .artifactId(HU_BLACKBELT_JUDO_META_EXPRESSION_OSGI)
+                        .versionAsInProject()),
+
+                mavenBundle(maven()
+                        .groupId(HU_BLACKBELT_JUDO_META)
+                        .artifactId(HU_BLACKBELT_JUDO_META_ASM_OSGI)
+                        .versionAsInProject()),
+
+                mavenBundle(maven()
+                        .groupId(HU_BLACKBELT_JUDO_META)
+                        .artifactId(HU_BLACKBELT_JUDO_META_PSM_OSGI)
+                        .versionAsInProject()),
+
+                mavenBundle(maven()
+                        .groupId(HU_BLACKBELT_JUDO_META)
+                        .artifactId(HU_BLACKBELT_JUDO_META_MEASURE_OSGI)
+                        .versionAsInProject()),
+
+                mavenBundle(maven()
+                        .groupId(HU_BLACKBELT_JUDO_META)
+                        .artifactId(HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_ASM)
+                        .versionAsInProject()),
+
+                mavenBundle(maven()
+                        .groupId(HU_BLACKBELT_JUDO_META)
+                        .artifactId(HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_PSM)
+                        .versionAsInProject()),
+
+                mavenBundle(maven()
+                        .groupId(HU_BLACKBELT_JUDO_META)
+                        .artifactId(HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_MEASURE)
+                        .versionAsInProject())
+
+        );
+    }
+
+    @Test
+    public void testBundleActive() {
+        assertNotNull(getBundle(bundleContext, HU_BLACKBELT_JUDO_META_EXPRESSION_OSGI));
+        assertEquals(Bundle.ACTIVE, getBundle(bundleContext, HU_BLACKBELT_JUDO_META_EXPRESSION_OSGI)
+                .getState());
+
+        assertNotNull(getBundle(bundleContext, HU_BLACKBELT_JUDO_META_ASM_OSGI));
+        assertEquals(Bundle.ACTIVE, getBundle(bundleContext, HU_BLACKBELT_JUDO_META_ASM_OSGI)
+                .getState());
+
+        assertNotNull(getBundle(bundleContext, HU_BLACKBELT_JUDO_META_PSM_OSGI));
+        assertEquals(Bundle.ACTIVE, getBundle(bundleContext, HU_BLACKBELT_JUDO_META_PSM_OSGI)
+                .getState());
+
+        assertNotNull(getBundle(bundleContext, HU_BLACKBELT_JUDO_META_MEASURE_OSGI));
+        assertEquals(Bundle.ACTIVE, getBundle(bundleContext, HU_BLACKBELT_JUDO_META_MEASURE_OSGI)
+                .getState());
+
+        assertNotNull(getBundle(bundleContext, HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_ASM));
+        assertEquals(Bundle.ACTIVE, getBundle(bundleContext, HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_ASM)
+                .getState());
+
+        assertNotNull(getBundle(bundleContext, HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_PSM));
+        assertEquals(Bundle.ACTIVE, getBundle(bundleContext, HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_PSM)
+                .getState());
+
+        assertNotNull(getBundle(bundleContext, HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_MEASURE));
+        assertEquals(Bundle.ACTIVE, getBundle(bundleContext, HU_BLACKBELT_JUDO_META_EXPRESSION_MODEL_ADAPTER_MEASURE)
+                .getState());
+
+    }
+}
