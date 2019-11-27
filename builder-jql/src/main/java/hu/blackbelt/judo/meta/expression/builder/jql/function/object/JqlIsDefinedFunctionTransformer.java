285d37e5b26ad0a05ec274ceda7ef967cf61ad43
Jozsef Borcsok <jozsef.borcsok@blackbelt.hu>
2019-11-27 16:03:05 +0100

    JNG-845: collections must be defined

--- builder-jql/src/main/java/hu/blackbelt/judo/meta/expression/builder/jql/function/object/JqlIsDefinedFunctionTransformer.java
+++ builder-jql/src/main/java/hu/blackbelt/judo/meta/expression/builder/jql/function/object/JqlIsDefinedFunctionTransformer.java
@@ -3,7 +3,6 @@ package hu.blackbelt.judo.meta.expression.builder.jql.function.object;
 import hu.blackbelt.judo.meta.expression.AttributeSelector;
 import hu.blackbelt.judo.meta.expression.Expression;
 import hu.blackbelt.judo.meta.expression.LogicalExpression;
-import hu.blackbelt.judo.meta.expression.ReferenceExpression;
 import hu.blackbelt.judo.meta.expression.builder.jql.JqlExpressionBuildingContext;
 import hu.blackbelt.judo.meta.expression.builder.jql.JqlTransformers;
 import hu.blackbelt.judo.meta.expression.builder.jql.function.AbstractJqlFunctionTransformer;
@@ -27,9 +26,9 @@ public class JqlIsDefinedFunctionTransformer extends AbstractJqlFunctionTransfor
         if (expression instanceof AttributeSelector) {
             AttributeSelector attributeSelector = (AttributeSelector) expression;
             comparisonExpression = newUndefinedAttributeComparisonBuilder().withAttributeSelector(attributeSelector).build();
-        } else if (expression instanceof ReferenceExpression) {
-            ReferenceExpression referenceExpression = (ReferenceExpression) expression;
-            comparisonExpression = newUndefinedNavigationComparisonBuilder().withReferenceExpression(referenceExpression).build();
+        } else if (expression instanceof ObjectNavigationExpression) {
+            ObjectNavigationExpression objectNavigationExpression = (ObjectNavigationExpression) expression;
+            comparisonExpression = newUndefinedNavigationComparisonBuilder().withObjectNavigationExpression(objectNavigationExpression).build();
         } else {
             throw new IllegalArgumentException("Not an attribute selector or object navigation: " + expression);
         }
