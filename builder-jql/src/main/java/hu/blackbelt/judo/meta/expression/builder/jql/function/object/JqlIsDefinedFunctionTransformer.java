48a027d417beeafc59553add76b4acd9aca09bca
Jozsef Borcsok <jozsef.borcsok@blackbelt.hu>
2021-02-04 12:00:38 +0100

    JNG-2408: support isDefined/isUndefined on (environment) variables

--- builder-jql/src/main/java/hu/blackbelt/judo/meta/expression/builder/jql/function/object/JqlIsDefinedFunctionTransformer.java
+++ builder-jql/src/main/java/hu/blackbelt/judo/meta/expression/builder/jql/function/object/JqlIsDefinedFunctionTransformer.java
@@ -2,6 +2,7 @@ package hu.blackbelt.judo.meta.expression.builder.jql.function.object;
 
 import static hu.blackbelt.judo.meta.expression.logical.util.builder.LogicalBuilders.newNegationExpressionBuilder;
 import static hu.blackbelt.judo.meta.expression.logical.util.builder.LogicalBuilders.newUndefinedAttributeComparisonBuilder;
+import static hu.blackbelt.judo.meta.expression.logical.util.builder.LogicalBuilders.newUndefinedEnvironmentVariableComparisonBuilder;
 import static hu.blackbelt.judo.meta.expression.logical.util.builder.LogicalBuilders.newUndefinedNavigationComparisonBuilder;
 
 import hu.blackbelt.judo.meta.expression.AttributeSelector;
@@ -11,6 +12,7 @@ import hu.blackbelt.judo.meta.expression.ObjectExpression;
 import hu.blackbelt.judo.meta.expression.builder.jql.ExpressionBuildingVariableResolver;
 import hu.blackbelt.judo.meta.expression.builder.jql.ExpressionTransformer;
 import hu.blackbelt.judo.meta.expression.builder.jql.function.AbstractJqlFunctionTransformer;
+import hu.blackbelt.judo.meta.expression.variable.EnvironmentVariable;
 import hu.blackbelt.judo.meta.jql.jqldsl.JqlFunction;
 
 public class JqlIsDefinedFunctionTransformer extends AbstractJqlFunctionTransformer<Expression> {
@@ -31,8 +33,11 @@ public class JqlIsDefinedFunctionTransformer extends AbstractJqlFunctionTransfor
         } else if (expression instanceof ObjectExpression) {
             ObjectExpression objectExpression = (ObjectExpression) expression;
             comparisonExpression = newUndefinedNavigationComparisonBuilder().withObjectNavigationExpression(objectExpression).build();
+        } else if (expression instanceof EnvironmentVariable) {
+            EnvironmentVariable environmentVariable = (EnvironmentVariable) expression;
+            comparisonExpression = newUndefinedEnvironmentVariableComparisonBuilder().withEnvironmentVariable(environmentVariable).build();
         } else {
-            throw new IllegalArgumentException("Not an attribute selector or object navigation: " + expression);
+            throw new IllegalArgumentException("Not an attribute selector, object navigation or environment variable: " + expression);
         }
         if (checkDefined) {
             return newNegationExpressionBuilder().withExpression(comparisonExpression).build();
