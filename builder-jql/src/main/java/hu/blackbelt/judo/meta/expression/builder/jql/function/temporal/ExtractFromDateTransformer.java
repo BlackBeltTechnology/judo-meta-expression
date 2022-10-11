3f6f6f77357f6061f6a447ed60b67cc5bbc9a4d3
Bence Gelei <54545024+bencegelei@users.noreply.github.com>
2022-10-11 12:04:33 +0200

    JNG-4140 Extend JSL-JQL functions (#90)
    * JNG-4141 Support div and mod on decimals
    
    * JNG-4141 Modulo added to measured adapter functions
    
    * [Release] Updating versions
    
    * JNG-4140 Transformer utils- and padding transformer added
    
    * JNG-4143, JNG-4144 fromseconds, toseconds for time and dayofweek and dayofyear for date
    
    * JNG-4145 Round with scale
    
    * JNG-4145 Round improvements
    
    * [Release] Updating versions
    
    * JNG-4140 Quality of life updates

---
+++ builder-jql/src/main/java/hu/blackbelt/judo/meta/expression/builder/jql/function/temporal/ExtractFromDateTransformer.java
@@ -0,0 +1,47 @@
+package hu.blackbelt.judo.meta.expression.builder.jql.function.temporal;
+
+/*-
+ * #%L
+ * JUDO :: Expression :: Model
+ * %%
+ * Copyright (C) 2018 - 2022 BlackBelt Technology
+ * %%
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is
+ * available at https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ * #L%
+ */
+
+import hu.blackbelt.judo.meta.expression.DateExpression;
+import hu.blackbelt.judo.meta.expression.Expression;
+import hu.blackbelt.judo.meta.expression.builder.jql.*;
+import hu.blackbelt.judo.meta.expression.builder.jql.function.AbstractJqlFunctionTransformer;
+import hu.blackbelt.judo.meta.expression.numeric.util.builder.ExtractDateExpressionBuilder;
+import hu.blackbelt.judo.meta.expression.temporal.DatePart;
+import hu.blackbelt.judo.meta.jql.jqldsl.JqlFunction;
+
+public class ExtractFromDateTransformer extends AbstractJqlFunctionTransformer<Expression> {
+
+    private final DatePart datePart;
+
+    public ExtractFromDateTransformer(ExpressionTransformer jqlTransformers, DatePart datePart) {
+        super(jqlTransformers);
+        this.datePart = datePart;
+    }
+
+    @Override
+    public Expression apply(Expression expression, JqlFunction functionCall, ExpressionBuildingVariableResolver context) {
+        DateExpression dateExpression = JqlTransformerUtils.castExpression(DateExpression.class, () -> expression, functionCall.getName() + " is not supported on {1}");
+        JqlTransformerUtils.validateParameterCount(functionCall.getParameters(), 0);
+        return ExtractDateExpressionBuilder.create().withDate(dateExpression).withPart(datePart).build();
+    }
+
+}
