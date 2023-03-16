5bb2858cbc459738af753acdfaec6fb8bde14bdb
gaborflorian <gabor.florian@blackbelt.hu>
2023-03-16 17:38:31 +0100

    Fix tabs and line ends

--- builder-jql/src/main/java/hu/blackbelt/judo/meta/expression/builder/jql/ExpressionBuildingVariableResolver.java
+++ builder-jql/src/main/java/hu/blackbelt/judo/meta/expression/builder/jql/ExpressionBuildingVariableResolver.java
@@ -9,13 +9,13 @@ package hu.blackbelt.judo.meta.expression.builder.jql;
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * This Source Code may also be made available under the following Secondary
  * Licenses when the conditions for such availability set forth in the Eclipse
  * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
  * with the GNU Classpath Exception which is
  * available at https://www.gnu.org/software/classpath/license.html.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  * #L%
  */
@@ -59,13 +59,13 @@ public interface ExpressionBuildingVariableResolver {
 
     Expression peekBaseExpression();
 
-	Collection<Variable> getVariables();
+    Collection<Variable> getVariables();
 
-	void removeVariable(Variable variable);
+    void removeVariable(Variable variable);
 
-	boolean resolveOnlyCurrentLambdaScope();
+    boolean resolveOnlyCurrentLambdaScope();
 
-	void pushVariableScope();
+    void pushVariableScope();
 
     void popVariableScope();
 }
